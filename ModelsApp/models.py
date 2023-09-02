0e294da97ae2f72e6f6fa92b055aeb84cfff078f
Ajeetyadav1994 <ajeetkumaryadav9721@gmail.com>
2023-09-02 11:53:04 +0530

    Create Employee Modles

--- ModelsApp/models.py
+++ ModelsApp/models.py
@@ -11,4 +11,12 @@ class Student(models.Model):
     def __str__(self):
         return self.name
 
+class Employee(models.Model):
+    name=models.CharField(max_length=50)
+    subject=models.CharField(max_length=50)
+    fee=models.IntegerField(blank=True,null=True)
+    department=models.CharField(max_length=100)
+    rate=models.IntegerField(blank=True,null=True)
+    def __str__(self):
+        return self.name
 
